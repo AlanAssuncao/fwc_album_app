46d91e5fda8228d16c6818f90c9196bcf3fdc99a
AlanAssuncao <alansoaresassuncao@gmail.com>
2022-10-12 19:03:31 -0300

    Dart Week: dia 1

---
+++ lib/app/fwc_album_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/theme/theme_config.dart';
+import 'package:fwc_album_app/app/pages/splash/splash_page.dart';
+import 'package:fwc_album_app/app/pages/splash/splash_route.dart';
+
+import 'pages/auth/login/login_page.dart';
+import 'pages/home/home_page.dart';
+
+class FwcAlbumApp extends StatelessWidget {
+  const FwcAlbumApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Fifa World Cup Album",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeConfig.theme,
+      routes: {
+        "/": (_) => const SplashRoute(),
+        "/auth/login": (_) => const LoginPage(),
+        "/home": (_) => const HomePage(),
+      },
+    );
+  }
+}
