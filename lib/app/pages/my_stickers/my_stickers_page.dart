920889a98718f0365e2956beda87eaa955fecd2d
AlanAssuncao <alansoaresassuncao@gmail.com>
2022-10-16 13:57:53 -0300

    Dart Week: dia 4

---
+++ lib/app/pages/my_stickers/my_stickers_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/pages/my_stickers/presenter/my_stickers_presenter.dart';
+import 'package:fwc_album_app/app/pages/my_stickers/view/my_stickers_view_impl.dart';
+import 'package:fwc_album_app/app/pages/my_stickers/widgets/sticker_group.dart';
+import 'package:fwc_album_app/app/pages/my_stickers/widgets/sticker_group_filter.dart';
+
+import 'widgets/sticker_status_filter.dart';
+
+class MyStickerPage extends StatefulWidget {
+  final MyStickersPresenter presenter;
+  const MyStickerPage({super.key, required this.presenter});
+
+  @override
+  State<MyStickerPage> createState() => _MyStickerPageState();
+}
+
+class _MyStickerPageState extends MyStickersViewImpl {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minhas figurinhas'),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                StickerStatusFilter(
+                  filterSelected: statusFilter,
+                ),
+                StickerGroupFilter(countries: countries),
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final group = album[index];
+                return StickerGroup(
+                  group: group,
+                  statusFilter: statusFilter,
+                );
+              },
+              childCount: album.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void updateStatusFilter(status) {
+    setState(() {
+      statusFilter = status;
+    });
+  }
+}
